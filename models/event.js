1eaa91adae558fb32e66dc879c90581897f8ac22
quantumsoul <prakhar24012000@gmail.com>
2022-07-27 01:17:56 +0530

    init commit

--- models/event.js
+++ models/event.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
-const jwt = require('jsonwebtoken')
 const eventSchema = new mongoose.Schema({
     eventTitle:{
         type: String,
