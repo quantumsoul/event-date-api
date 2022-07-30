0adaaf511b09c123521b208283e06de4af11b1d1
quantumsoul <prakhar24012000@gmail.com>
2022-07-30 22:08:24 +0530

    new api added

--- routes/route.js
+++ routes/route.js
@@ -36,4 +36,45 @@ router.get("/v2/get/event",async(req,res)=>{
         res.status(400)
     }
 });
+router.patch("/edit/event",async(req,res)=>{
+    try {
+        const event = await Event.findByIdAndUpdate(    
+            req.query.id,
+            { $set: req.body },
+            { new: true }
+        )
+        res.send(event)
+    } catch (error) {
+        res.status(400)
+    }
+});
+router.delete("/delete/event",async(req,res)=>{
+    try {
+        const event = await Event.findByIdAndDelete(req.query.id)
+        res.send(event)
+    } catch (error) {
+        res.status(400)
+    }
+});
+router.post("/v2/edit/event",async(req,res)=>{
+    try {
+        const event = await EventCapacity.findByIdAndUpdate(    
+            req.query.id,
+            { $set: req.body },
+            { new: true }
+        )
+        await event.save()
+        res.send(event)
+    } catch (error) {
+        res.status(400)
+    }
+});
+router.get("/v2/delete/event",async(req,res)=>{
+    try {
+        const event = await EventCapacity.findByIdAndDelete(req.query.id)
+        res.send(event)
+    } catch (error) {
+        res.status(400)
+    }
+});
 module.exports = router
