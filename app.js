53508605fa04e6090e59d9ae81d500fc52f6996f
quantumsoul <prakhar24012000@gmail.com>
2022-08-04 21:11:47 +0530

    bug fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     // allow different header field 
     res.header('Access-Control-Allow-Headers', '*');
-    res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS, PATCH, DELETE');
   
     next();
   })
